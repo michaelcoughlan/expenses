5887fbd0672d71223e11293f21e92b3d4a09b387
michaelcoughlan <m.coughlan18@gmail.com>
2020-10-08 12:22:34 +0100

    Update path to app and __str__ function in the Row model

--- sheet/models.py
+++ sheet/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from datetime import datetime
 
 # Create your models here.
 class Row(models.Model):
@@ -7,5 +8,7 @@ class Row(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
+    readonlyfields = ['created_at', 'updated_at']
+
     def __str__(self):
-        return "Amount: {}, Date: {}".format((self.income - self.expenditure), self.created_at)
+        return "Amount: {} ({})".format((self.income - self.expenditure), self.created_at.strftime('%d-%m-%Y'))
