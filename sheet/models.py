bfcb7c39d17fd7449981c084794683218135dddd
michaelcoughlan <m.coughlan18@gmail.com>
2020-10-08 11:56:49 +0100

    Add the row model

--- sheet/models.py
+++ sheet/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Row(models.Model):
+    income = models.IntegerField()
+    expenditure = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return "Amount: {}, Date: {}".format((self.income - self.expenditure), self.created_at)
